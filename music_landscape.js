6481582bd95cf1215bce4539fb7e8634b9303539
NickQwQ-sudo <nick.yu.ming@gmail.com>
2025-09-07 21:52:35 +1200

    Try 1

--- music_landscape.js
+++ music_landscape.js
@@ -1,61 +1,35 @@
 
 
 function draw_one_frame(words, vocal, drum, bass, other,counter) {
-  background(150, 200, 255);
-  drawMushroomHouse(100, height - 200);
-  fill(255); 
-  noStroke();
-  ellipse(100, 100, 60, 60);
-  ellipse(130, 90, 80, 80);
-  ellipse(170, 100, 60, 60);
+  background(200);
+  translate(width/2, height/2);
+  colorMode(HSB, 360, 100, 100);
+  strokeWeight(2);
+  noFill();
 
-  ellipse(400, 150, 50, 50);
-  ellipse(430, 140, 70, 70);
-  ellipse(470, 150, 50, 50);
+  let totalBars = 180; 
+  let innerRadius = 150;
+  let maxLineLength = 200;
 
-function drawMushroomHouse(x, y) {
-  push();
-  translate(x, y);
+  for (let i = 0; i < totalBars; i++) {
+    let angle = map(i, 0, totalBars, 0, TWO_PI);
+    let value = map(other, 0, 100, 0, 1);  
+    let fftValue = noise(counter * 0.01 + i) * value * maxLineLength;
 
-  scale(1.8); 
+    let x1 = cos(angle) * innerRadius;
+    let y1 = sin(angle) * innerRadius;
+    let x2 = cos(angle) * (innerRadius + fftValue);
+    let y2 = sin(angle) * (innerRadius + fftValue);
 
-  
-  fill(255);
-  rect(-30, -60, 60, 60, 20);
-
-  
-  fill(150, 75, 0); 
-  rect(-10, -30, 20, 40, 5);
-  fill(255, 215, 0); 
-  ellipse(5, -10, 5, 5);
-
-  
-  fill(220, 0, 0);
-  ellipse(0, -70, 100, 60); 
-
- 
-  fill(255);
-  ellipse(-25, -75, 15);
-  ellipse(20, -78, 10);
-  ellipse(0, -65, 12);
-
-  pop();
-}
-
-  
-  let grassHeight = 200;  
-fill(76, 175, 80);       
-rect(0, height - grassHeight, width, grassHeight);
-
-
-  let tileSize = 40;
-  for (let x = 0; x < width; x += tileSize) {
-    fill(139, 69, 19);
-    rect(x, height - tileSize, tileSize, tileSize, 8);
-
-    
-    fill(34, 139, 34); 
-    ellipse(x + tileSize / 2, height - grassHeight, tileSize * 0.9, tileSize * 0.3);
+    let hue = map(i, 0, totalBars, 0, 360);
+    stroke(hue, 100, 100);
+    line(x1, y1, x2, y2);
   }
 
- }
+  
+  noStroke();
+  fill(0);
+  circle(0, 0, innerRadius * 2);
+}
+
+
