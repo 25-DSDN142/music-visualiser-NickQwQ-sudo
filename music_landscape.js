f9d3aeee891dcff90ddf93388bff43e4b5f00693
NickQwQ-sudo <nick.yu.ming@gmail.com>
2025-09-08 00:35:02 +1200

    layer 2.1

--- music_landscape.js
+++ music_landscape.js
@@ -16,17 +16,17 @@ for (let i = 0; i < 200; i++) {
 }
 
 
+
 push();
 translate(width / 2, height / 2);
 
 let numRays = 180;
-let baseRadius = 200;
+let baseRadius = 120;
 strokeWeight(2);
 colorMode(HSB, 360, 100, 100);
 
-
 if (counter > 0) {
-  rotationAngle += 0.01;  
+  rotationAngle += 0.01; 
 }
 rotate(rotationAngle);
 
@@ -35,17 +35,22 @@ for (let i = 0; i < numRays; i++) {
 
   
   let wave = map(bass, 0, 100, 0, 80);
-  let dynamicRadius = baseRadius + wave;
+  let dynamicRadius = baseRadius + wave * (0.5 + sin(i * 0.3 + counter * 0.15));
 
   let x = cos(angle) * dynamicRadius;
   let y = sin(angle) * dynamicRadius;
 
   stroke(map(i, 0, numRays, 0, 360), 80, 100);
   line(0, 0, x, y);
+
+  let alpha = map(drum, 0, 100, 30, 100);  
+  stroke(map(i, 0, numRays, 0, 360), 80, 100, alpha);
+  line(0, 0, x, y);
 }
 
 colorMode(RGB);
 pop();
+
 
 
 
