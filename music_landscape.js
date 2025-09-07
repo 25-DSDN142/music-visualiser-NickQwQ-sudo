7ded65b8b49351685a37be9b0b4675eaa37aed18
NickQwQ-sudo <nick.yu.ming@gmail.com>
2025-09-07 21:58:00 +1200

    layer 1

--- music_landscape.js
+++ music_landscape.js
@@ -1,35 +1,18 @@
 
 
 function draw_one_frame(words, vocal, drum, bass, other,counter) {
-  background(200);
-  translate(width/2, height/2);
-  colorMode(HSB, 360, 100, 100);
-  strokeWeight(2);
-  noFill();
+  background(0);
 
-  let totalBars = 180; 
-  let innerRadius = 150;
-  let maxLineLength = 200;
 
-  for (let i = 0; i < totalBars; i++) {
-    let angle = map(i, 0, totalBars, 0, TWO_PI);
-    let value = map(other, 0, 100, 0, 1);  
-    let fftValue = noise(counter * 0.01 + i) * value * maxLineLength;
-
-    let x1 = cos(angle) * innerRadius;
-    let y1 = sin(angle) * innerRadius;
-    let x2 = cos(angle) * (innerRadius + fftValue);
-    let y2 = sin(angle) * (innerRadius + fftValue);
-
-    let hue = map(i, 0, totalBars, 0, 360);
-    stroke(hue, 100, 100);
-    line(x1, y1, x2, y2);
-  }
-
-  
+for (let i = 0; i < 200; i++) {
+  let x = noise(i, counter * 0.01) * width;
+  let y = noise(i + 1000, counter * 0.01) * height;
+  let alpha = map(sin(counter * 0.05 + i), -1, 1, 50, 255);
   noStroke();
-  fill(0);
-  circle(0, 0, innerRadius * 2);
+  fill(255, alpha);
+  ellipse(x, y, 1.5, 1.5);
+}
+
 }
 
 
